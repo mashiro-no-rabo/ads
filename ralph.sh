c81dc3341602b43f297eb6da6e6969fd366d4e40
LOU Xun <aquarhead@gmail.com>
2026-03-25 22:54:12 +0000

    NON-RALPH: minor fix

--- ralph.sh
+++ ralph.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 
-RALPH=1 while :; do cat ralph.md | claude --dangerously-skip-permissions ; sleep 5; done
+while :; do cat ralph.md | RALPH=1 claude --dangerously-skip-permissions ; sleep 5; done
